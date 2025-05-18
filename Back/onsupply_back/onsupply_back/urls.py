28cea541a63aa0430eed43353805296b353ed576
Pawin <pawin.sukta@gmail.com>
2025-05-18 10:18:58 +0700

    update

--- Back/onsupply_back/onsupply_back/urls.py
+++ Back/onsupply_back/onsupply_back/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.http import HttpResponse
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('auth_service.urls')),
     path('api/', include('course_service.urls')),
+    path('', lambda request: HttpResponse("Hello from Django backend!"))
 ]
 
 if settings.DEBUG:
